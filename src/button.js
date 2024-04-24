7ba012106ccd331ab88a6761bc8a019a2cd51c9a
Swapnil Dubey <100476652+swapnildubey29@users.noreply.github.com>
2024-04-24 14:07:27 +0530

    Update button.js

--- src/button.js
+++ src/button.js
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+
+const Button = () => {
+  const [toggle1, setToggle1] = useState(false);
+  const [toggle2, setToggle2] = useState(false);
+
+  const handleToggle1 = () => {
+    setToggle1(!toggle1);
+  };
+
+  const handleToggle2 = () => {
+    setToggle2(!toggle2);
+  };
+
+
+  return (
+   
+      <div className="left-section">
+        <div className={`toggle ${toggle1 ? 'active' : ''}`} onClick={handleToggle1}></div>
+        <div className={`toggle ${toggle2 ? 'active' : ''}`} onClick={handleToggle2}></div>
+      </div>
+  );
+};
+
+export default Button;
 
